45c7730ae42d5e4e44b0e132bc15be89126ebd59
Elliot Weishaar <elliotweishaar27@gmail.com>
2023-12-16 15:12:47 -0600

    new init

--- flake.nix
+++ flake.nix
@@ -3,34 +3,16 @@
   
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-      home-manager.url = "github:nix-community/home-manager/release-23.11";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+      home-manager = { 
+        url = "github:nix-community/home-manager/release-23.11";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}: 
+  outputs = { self, nixpkgs, home-manager, ...}@inputs: 
     let
-      # ---- SYSTEM SETTINGS ---- #
       system = "x86_64-linux";
-      hostname = "zendikar";
-      profile = "zendikar"; # select a profile defined in the profiles directory
-      timezone = "US/Central"; 
-      locale = "en_US.UTF-8";
-      nfs-server = "10.0.0.8";
-
-      # ---- USER SETTINGS ---- #
-      username = "eweishaar";
-      name = "Elliot Weishaar";
-      email = "elliotweishaar27@gmail.com";
-      dotfilesDir = "~/.dotfiles";
-      theme = "nord";
-      wm = "hyprland";
-      wmType = "wayland";
-      browser = "brave";
-      editor = "vim";
-      term = "alacritty";
-      font = "Fira Code Nerdfont";
-      fontPkg = pkgs.fira-code-nerdfont;
-
       pkgs = nixpkgs.legacyPackages.${system};
       lib = nixpkgs.lib;
     in {
