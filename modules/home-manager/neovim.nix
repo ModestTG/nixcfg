86cca8c8dc65cc929da10c3e1aab3db200647984
Elliot Weishaar <elliotweishaar27@gmail.com>
2024-03-30 19:33:54 -0500

    update nixpkgs

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -4,7 +4,8 @@
   home.file.".config/nvim" = {
     source = builtins.fetchGit {
       url = "https://github.com/ModestTG/kickstart";
-      rev = "";
+      rev = "38ff50d394b050b63bef02d9357ac352aafc42f8";
+    };
     recursive = true;
   };
 }
