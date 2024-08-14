60f302cc67544a8b64962a0f86cd388e2b633bd4
ModestTG <ssh@mailserver.com>
2024-08-13 22:01:15 -0500

    fix: updated nix maintenance. Added auto upgrade

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -1,21 +1,33 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, inputs, ... }:
 
 {
+  system.autoUpgrade = {
+    enable = true;
+    dates = "01:00";
+    flake = inputs.self.outPath;
+    flags = [ "--update-input" "nixpkgs" "-L" ];
+    randomizedDelaySec = "30min";
+  };
   nix = {
     package = pkgs.nixFlakes;
     settings = {
-      auto-optimise-store = true;
       connect-timeout = 5;
       log-lines = 25;
       min-free = 128000000; # 128MB
       max-free = 1000000000; # 1GB
       warn-dirty = false;
       experimental-features = [ "nix-command" "flakes" ];
+
     };
     gc = {
-      automatic = lib.mkDefault true;
-      dates = lib.mkDefault "weekly";
-      options = lib.mkDefault "--delete-older-than 7d";
+      automatic = true;
+      dates = "02:00";
+      options = "--delete-older-than 14d";
+      randomizedDelaySec = "10min";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "03:00" ];
     };
   };
   nixpkgs.config.allowUnfree = lib.mkForce true;
