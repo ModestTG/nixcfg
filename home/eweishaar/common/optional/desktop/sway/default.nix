ae4323bdd428712f3343ccedd60d4e928b6cfea7
ModestTG <ssh@mailserver.com>
2024-08-18 09:30:11 -0500

    feat: added base16 color scheming

--- home/eweishaar/common/optional/desktop/sway/default.nix
+++ home/eweishaar/common/optional/desktop/sway/default.nix
@@ -1,7 +1,10 @@
-{ pkgs, userlib, ... }:
+{ pkgs, userlib, inputs, ... }:
 
 {
-  imports = userlib.scanPaths ./.;
+  imports = [
+    inputs.base16.homeManagerModule
+    { scheme = "${inputs.tt-schemes}/base16/tokyo-night-terminal-dark.yaml"; }
+  ] ++ (userlib.scanPaths ./.);
 
   home.packages = with pkgs; [
     brave
