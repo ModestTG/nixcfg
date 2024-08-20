a48cb698971b09ad6911e17108f0149650a1d9fd
ModestTG <ssh@mailserver.com>
2024-08-20 15:25:09 -0500

    fix: typo

--- home/eweishaar/common/optional/desktop/lazygit.nix
+++ home/eweishaar/common/optional/desktop/lazygit.nix
@@ -2,6 +2,6 @@
 
 {
   home.packages = with pkgs; [ lazygit ];
-  xdg.configFile."lazygit.config.yml".source =
+  xdg.configFile."lazygit/config.yml".source =
     "${inputs.theme_tokyonight}/extras/lazygit/tokyonight_night.yml";
 }
