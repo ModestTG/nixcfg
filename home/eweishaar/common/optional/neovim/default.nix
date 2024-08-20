53413ab7d5bb6952b6ef8f1e9c2cd26fea0e68a1
ModestTG <ssh@mailserver.com>
2024-08-20 12:05:49 -0500

    fix: switch nvim config load from home.file to xdg.configFile

--- home/eweishaar/common/optional/neovim/default.nix
+++ home/eweishaar/common/optional/neovim/default.nix
@@ -1,14 +1,9 @@
-{ pkgs, userlib, lib, ... }:
+{ pkgs, userlib, ... }:
 
-let
-  # initLuaStringInterpolation =
-  #   builtins.replaceStrings [ "cmd = {'nil'}" ] [ "cmd = {${pkgs.nil}}" ]
-  #   (builtins.readFile (userlib.relativeToRoot
-  #     "home/eweishaar/common/optional/neovim/config/init.lua"));
-in {
+{
   home.packages = with pkgs; [ neovim nixfmt rustc cargo ];
 
-  home.file.".config/nvim/" = {
+  xdg.configFile."nvim" = {
     source =
       userlib.relativeToRoot "home/eweishaar/common/optional/neovim/config";
     recursive = true;
