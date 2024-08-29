44cac00df122335fc854597baf8ed14ffb4ffff9
ModestTG <ssh@mailserver.com>
2024-08-28 22:20:13 -0500

    feat: added nixfmt-rfc-style for updated formatting

--- home/eweishaar/common/optional/neovim/default.nix
+++ home/eweishaar/common/optional/neovim/default.nix
@@ -1,11 +1,15 @@
 { pkgs, userlib, ... }:
 
 {
-  home.packages = with pkgs; [ neovim nixfmt-rfc-style rustc cargo ];
+  home.packages = with pkgs; [
+    neovim
+    nixfmt-rfc-style
+    rustc
+    cargo
+  ];
 
   xdg.configFile."nvim" = {
-    source =
-      userlib.relativeToRoot "home/eweishaar/common/optional/neovim/config";
+    source = userlib.relativeToRoot "home/eweishaar/common/optional/neovim/config";
     recursive = true;
   };
 }
