f10c177f7508737f42fc39bc6660970731716eec
ModestTG <ssh@mailserver.com>
2024-12-19 08:07:29 -0600

    fix: move dominaria specific ssh config to root home-manager config

--- home/eweishaar/common/core/ssh.nix
+++ home/eweishaar/common/core/ssh.nix
@@ -1,11 +1,6 @@
-{ config, pkgs, ... }:
-
 {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      IdentityFile = ~/.ssh/id_dominaria
-    '';
     addKeysToAgent = "yes";
   };
 
--- home/eweishaar/dominaria.nix
+++ home/eweishaar/dominaria.nix
@@ -19,4 +19,35 @@
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = "calc.desktop";
     };
   };
+  programs.ssh = {
+    matchBlocks = {
+      "mirrodin" = {
+        hostname = "10.0.20.22";
+        user = "eweishaar";
+      };
+      "jace-master-0" = {
+        hostname = "10.0.20.10";
+        user = "eweishaar";
+      };
+      "jace-master-1" = {
+        hostname = "10.0.20.11";
+        user = "eweishaar";
+      };
+      "jace-master-2" = {
+        hostname = "10.0.20.12";
+        user = "eweishaar";
+      };
+      "jace-worker-0" = {
+        hostname = "10.0.20.13";
+        user = "eweishaar";
+      };
+      "jace-worker-1" = {
+        hostname = "10.0.20.14";
+        user = "eweishaar";
+      };
+    };
+    extraConfig = ''
+      IdentityFile ~/.ssh/id_dominaria
+    '';
+  };
 }
