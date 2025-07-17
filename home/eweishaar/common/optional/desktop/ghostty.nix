88279f87e760d634865bd6803fb0245f0e4c9d58
ModestTG <ssh@mailserver.com>
2025-07-17 09:22:19 -0500

    fix: update ghostty settings

--- home/eweishaar/common/optional/desktop/ghostty.nix
+++ home/eweishaar/common/optional/desktop/ghostty.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ ghostty ];
-  xdg.configFile."ghostty/config".text = ''
-    font-family = "FiraCode Nerd Font Propo"
-    theme = tokyonight
-    background-opacity = 0.9
-    window-decoration = false
-    gtk-single-instance = true
-    confirm-close-surface = false
-  '';
+  programs.ghostty.settings = {
+    font-family = "FiraCode Nerd Font Propo";
+    theme = "tokyonight";
+    background-opacity = 0.9;
+    window-decoration = false;
+    gtk-single-instance = true;
+    confirm-close-surface = false;
+  };
 }
