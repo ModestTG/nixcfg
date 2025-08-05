ef504ce48c63447a8364bda5318056468b8ba548
ModestTG <ssh@mailserver.com>
2025-08-05 15:30:59 -0500

    fix: NFS startup issues

--- nixosModules/nfs.nix
+++ nixosModules/nfs.nix
@@ -13,18 +13,24 @@
   config = lib.mkIf config.nixosModule.fs.nfs.enable {
     environment.systemPackages = with pkgs; [ nfs-utils ];
     services.rpcbind.enable = true;
+    boot.supportedFilesystems = [ "nfs" ];
     fileSystems =
       let
         nfsServer = "10.0.0.8";
+        options = [
+          "x-systemd.automount"
+          "noauto"
+        ];
+        fsType = "nfs";
       in
       {
         "/mnt/k8s-nfs" = {
           device = nfsServer + ":/mnt/AuxPool/K8S-NFS";
-          fsType = "nfs";
+          inherit fsType options;
         };
         "/mnt/plexpool" = {
           device = nfsServer + ":/mnt/PlexPool";
-          fsType = "nfs";
+          inherit fsType options;
         };
       };
   };
