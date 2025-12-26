8dbe5fa4dcffe9dabf814454122325942fe66113
ModestTG <ssh@mailserver.com>
2025-12-26 13:25:56 -0600

    chore: upgrade

--- flake.nix
+++ flake.nix
@@ -42,7 +42,10 @@
       url = "github:zhaofengli/colmena";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    agenix.url = "github:ryantm/agenix";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
