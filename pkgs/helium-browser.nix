557b9cdf99efbd6eaee44ea09ba11d8ce457007c
ModestTG <ssh@mailserver.com>
2026-01-21 09:02:47 -0600

    chore: upgrade

--- pkgs/helium-browser.nix
+++ pkgs/helium-browser.nix
@@ -1,12 +1,11 @@
 { appimageTools }:
 let
   pname = "helium-browser";
-  version = "0.7.9.1";
+  version = "0.8.2.1";
   arch = "x86_64";
   src = builtins.fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-${arch}.AppImage";
-    sha256 = "08vgld73dyzf6yg9rswxqza6rnii1ck97wvjha3ly9jgs9sbrp7b";
-
+    sha256 = "0vvmk8ljhql10mlx8mhlyza534155cqxkf6ii4m66iwshnklgcv9";
   };
   appImageContents = appimageTools.extract { inherit pname version src; };
 in
